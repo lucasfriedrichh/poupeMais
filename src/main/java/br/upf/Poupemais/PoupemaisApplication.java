7a469cd7321f69cdca6e03eb297d231fc93fda6f
Lucas M. T. Friedrich <frielucas@gmail.com>
2023-03-30 11:41:48 -0300

    Initial commit

---
+++ src/main/java/br/upf/Poupemais/PoupemaisApplication.java
@@ -0,0 +1,13 @@
+package br.upf.Poupemais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoupemaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoupemaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/upf/Poupemais/PoupemaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upf.Poupemais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoupemaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
